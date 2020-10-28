f6c1153d8fc6922ec235e20e8fc00570a7c70fb6
Kay Holten <holtenkay@gmail.com>
2020-10-28 10:07:49 -0700

    adds functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,93 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AddBookmark from './AddBookmark/AddBookmark';
+import BookmarkApp from './BookmarkApp/BookmarkApp';
+
+
+const bookmarks = [
+  {
+  title:"Google",
+  url:"http://www.google.com", 
+  rating:"3", 
+  description:"No evil"
+  },
+  {
+    title:"Google",
+    url:"http://www.google.com", 
+    rating:"3", 
+    description:"No evil"
+  }
+];
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        // Add your key after Bearer
+        "Authorization": "Bearer $2a$10$L3uDnsfSUNRGjWm9.7tPjOzr30yTNkvM1UT22KCbFEogJciM5G9H6",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+  }
+
+  setShowAddForm(show) {
+    this.setState({
+      showAddForm: show
+    });
+  }
+
+  addBookmark(bookmark) {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark],
+      showAddForm: false
+    });
+  }
+
   render() {
+    const page = this.state.showAddForm
+      ? <AddBookmark 
+          showForm={show => this.setShowAddForm(show)}
+          handleAdd={bookmark => this.addBookmark(bookmark)}
+      />
+      : <BookmarkApp 
+        bookmarks={this.state.bookmarks}
+        showForm={show => this.setShowAddForm(show)}
+      />
+
     return (
       <div className="App">
+        {page}
       </div>
     );
   }
